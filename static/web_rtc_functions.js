6732319519c715c0ee47f5d842fb079e1b2fedd7
daltonkdwyer <dalton.dwyer@gmail.com>
2023-02-21 18:41:34 -0800

    a

--- static/web_rtc_functions.js
+++ static/web_rtc_functions.js
@@ -77,6 +77,8 @@ export async function create_RTCP_answer(remote_offer_SDP){
         console.log("Connection state change: ", peerConnection.connectionState)
     };
 
+    await waitForAllICE(peerConnection)
+
     console.log("This should now have both an answer and an offer:")
     console.log(peerConnection)
 
