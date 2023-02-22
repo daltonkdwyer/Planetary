111f14d56c34c8b28f4916e8136e2061b10e975e
daltonkdwyer <dalton.dwyer@gmail.com>
2023-02-21 18:59:36 -0800

    a

--- app.py
+++ app.py
@@ -27,9 +27,9 @@ def home():
     return render_template("index.html")
 
 @app.route('/dictionary', methods=['GET'])
-def home():
+def dictionary_print():
     print(room_dict)
-    return ("bannana")
+    return render_template("index.html")
 
 # Servers the first car page. If a client tries to get here, this is what they'll get
 @app.route('/rc_car1', methods=['GET'])
