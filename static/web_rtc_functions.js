9262f1c3957e0e629d6e76875d7fa889829d68f5
daltonkdwyer <dalton.dwyer@gmail.com>
2023-02-22 18:11:06 -0800

    vvv

--- static/web_rtc_functions.js
+++ static/web_rtc_functions.js
@@ -77,8 +77,6 @@ export async function create_RTCP_answer(remote_offer_SDP){
         console.log("Connection state change: ", peerConnection.connectionState)
     };
 
-    await waitForAllICE(peerConnection)
-
     console.log("This should now have both an answer and an offer:")
     console.log(peerConnection)
 
@@ -135,5 +133,3 @@ function send_ICE_candidates(e){
         send_ICE_candidates_socket(new_ice_candidate)
     }
 }
-
-// Test I am the reverted files
