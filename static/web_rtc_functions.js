5f06d6c8f89dc27f98f7be4bae1614ceedebb939
daltonkdwyer <dalton.dwyer@gmail.com>
2023-02-22 17:40:48 -0800

    b

--- static/web_rtc_functions.js
+++ static/web_rtc_functions.js
@@ -9,21 +9,21 @@ const servers = {
         {
           urls: "stun:stun.l.google.com:19302",
         },
-        {
-          urls: "turn:relay.metered.ca:80",
-          username: "999c14afe3cc4008b72f3aa0",
-          credential: "oBpkY5NWEwvTK/gc",
-        },
-        {
-          urls: "turn:relay.metered.ca:443",
-          username: "999c14afe3cc4008b72f3aa0",
-          credential: "oBpkY5NWEwvTK/gc",
-        },
-        {
-          urls: "turn:relay.metered.ca:443?transport=tcp",
-          username: "999c14afe3cc4008b72f3aa0",
-          credential: "oBpkY5NWEwvTK/gc",
-        },
+        // {
+        //   urls: "turn:relay.metered.ca:80",
+        //   username: "999c14afe3cc4008b72f3aa0",
+        //   credential: "oBpkY5NWEwvTK/gc",
+        // },
+        // {
+        //   urls: "turn:relay.metered.ca:443",
+        //   username: "999c14afe3cc4008b72f3aa0",
+        //   credential: "oBpkY5NWEwvTK/gc",
+        // },
+        // {
+        //   urls: "turn:relay.metered.ca:443?transport=tcp",
+        //   username: "999c14afe3cc4008b72f3aa0",
+        //   credential: "oBpkY5NWEwvTK/gc",
+        // },
     ],
   };
 
