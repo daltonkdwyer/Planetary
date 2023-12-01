999d229a62ad1644056de9e922820508c632eab0
daltonkdwyer <dalton.dwyer@gmail.com>
2023-12-01 17:57:53 -0500

    a

--- static/index.js
+++ static/index.js
@@ -1,6 +1,9 @@
 // This is just the home page you can use to get to the cars
 
-let car1_socket_server = io('https://plntry33.ngrok.io')
+let car1_socket_server
+
+car1_socket_server = io('https://plntry33.ngrok.io');
+
 const car1status = document.getElementById("Car1 status")
 
 car1_socket_server.on('connect', function(){
