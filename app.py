c015a8590aeb347f82f70c3d36063eb109a50777
daltonkdwyer <dalton.dwyer@gmail.com>
2023-12-16 14:25:44 -0500

    a

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def logs():
         server_message = "Heroku Log Message"
         server_data = {"Request: ": data1, "Request.form: ": data2, "Request.form.todict()": data3, "Variable type of request.form": type_var}
         server_payload = {"Message": server_message, "Data": server_data}
-        socket.send(server_payload)
+        # socket.send(server_payload)
         return jsonify(server_data)
     
     # if request.method == 'GET':
