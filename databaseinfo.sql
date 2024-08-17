aa0a9e022f86b6fa9fa74f48ad3e91b919351872
daltonkdwyer <dalton.dwyer@gmail.com>
2024-08-16 20:45:21 -0400

    a

---
+++ databaseinfo.sql
@@ -0,0 +1,13 @@
+CREATE TABLE DRIVE_DURATIONS1
+    (id SERIAL PRIMARY KEY,
+    user_name VARCHAR(255),
+    vehicle_type VARCHAR(255),
+    vehicle_ID NUMERIC,
+    duration NUMERIC);
+
+-- To talk with the heroku database, use this command and then type in SQL: heroku pg:psql postgresql-acute-47165 --app plntry
+
+INSERT INTO DRIVE_DURATIONS1 (id, user_name, vehicle_type, vehicle_ID, duration)
+VALUES (1, 'plntry_ctrl_1', 'rc_car', 1, 100);
+
+SELECT * FROM DRIVE_DURATIONS1;
